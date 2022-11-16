16b17ce2ec8e595a9816d1c61f5b769fd284155d
Stefan Rusev <StefanGRusev91@gmail.com>
2022-11-16 16:01:24 +0200

    Inheritance-Lab

---
+++ Inheritance-Lab/05.StackOfStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.AddRange(new List<string>
+            {
+               "abc", "bbc", "aaa"
+            });
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
