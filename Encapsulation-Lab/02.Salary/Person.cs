f84272355e4944d05e6de9d7ace5543bfb299936
Stefan Rusev <StefanGRusev91@gmail.com>
2022-11-22 14:09:26 +0200

    Encapsulation-Lab

---
+++ Encapsulation-Lab/02.Salary/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+           
+        }
+    }
+}
