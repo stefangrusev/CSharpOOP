d5fdd01904cf4073a41eada2c2a6ad61f4ea69b1
Stefan Rusev <StefanGRusev91@gmail.com>
2022-11-28 22:29:13 +0200

    InterfacesAndAbstraction-Lab

---
+++ InterfacesAndAbstraction-Lab/01.Shapes/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Shapes
+{
+    class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.width, '*', '*');
+            for (int i = 1; i < this.height - 1; ++i)
+            {
+                DrawLine(this.width, '*', ' ');
+            }
+            DrawLine(this.width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width; ++i)
+            {
+                Console.Write(mid);
+            }
+            Console.WriteLine(end);
+        }
+    }
+}
